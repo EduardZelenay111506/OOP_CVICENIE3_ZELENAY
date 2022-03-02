7c85bf2dd08e245258d8a22660cf5d287242042e
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-02 19:25:46 +0100

    metody, gettre/settre

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,5 @@
 package sk.stuba.fei.uim.oop;
-
 import sk.stuba.fei.uim.oop.auto.Auto;
-
 public class Main {
     public static void main(String[] args) {
         Auto skoda = new Auto();
@@ -9,6 +7,11 @@ public class Main {
 
         skoda.jedna = 1;
         bmw.jedna = 2;
+        skoda.setStavNadrze(100);
+        skoda.setKapacitaNadrze(100);
+        System.out.println(skoda.stav());
+        skoda.jazdi(100);
+        System.out.println(skoda.stav());
 
         if (skoda.jedna == bmw.jedna) {
             System.out.println("rovnake");
--- src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -9,4 +9,51 @@ public class Auto {
     int dva;
     protected int tri;
     private int styri;
+    public static final double PALIVO_NA_KM = 0.2;
+    private double kapacitaNadrze;
+    private double stavNadrze;
+    private boolean ojazdene;
+
+    public void jazdi(double vzdialenost) {
+        this.ojazdene = true;
+        stavNadrze -= vzdialenost * PALIVO_NA_KM;
+        if (stavNadrze < 0) {
+            stavNadrze = 0;
+        }
+    }
+
+    public String stav() {
+        String vysledok;
+        if (this.ojazdene) {
+            vysledok = "Auto je ojazdene";
+        } else {
+            vysledok = "Auto je neojazdene";
+        }
+        vysledok += " * stav nadrze: " + stavNadrze + "/" + kapacitaNadrze;
+        return vysledok;
+    }
+
+    public double getKapacitaNadrze() {
+        return kapacitaNadrze;
+    }
+
+    public void setKapacitaNadrze(double kapacitaNadrze) {
+        this.kapacitaNadrze = kapacitaNadrze;
+    }
+
+    public double getStavNadrze() {
+        return stavNadrze;
+    }
+
+    public void setStavNadrze(double stavNadrze) {
+        this.stavNadrze = stavNadrze;
+    }
+
+    public boolean isOjazdene() {
+        return ojazdene;
+    }
+
+    public void setOjazdene(boolean ojazdene) {
+        this.ojazdene = ojazdene;
+    }
 }
